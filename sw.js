9d646aea786530da26039bbd3d42b70719145d40
Eminence5070 <119481238+unleashmysigmapowers@users.noreply.github.com>
2024-11-08 03:14:12 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,21 +18,40 @@ self.addEventListener('install', (event) => {
     );
 });
 
-
+// self updating, 
 self.addEventListener('fetch', (event) => {
-    // event.preventDefault()
+    const { request } = event;
+
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then((networkResponse) => {
-                    return caches.open(CACHE_NAME).then((cache) => {
-                        cache.put(event.request, networkResponse.clone());
+        caches.match(request).then((cachedResponse) => {
+            // dont update index.html, just in case it lets goguardian in
+            if (request.url.endsWith('/index.html') && cachedResponse) {
+                return cachedResponse;
+            }
+
+            // fetch and update the cache if the response differs
+            return fetch(request).then((networkResponse) => {
+                if (cachedResponse && networkResponse.ok) {
+                    const cachedText = cachedResponse.clone().text();
+                    const networkText = networkResponse.clone().text();
+
+                    return Promise.all([cachedText, networkText]).then(([cachedData, networkData]) => {
+                        if (cachedData !== networkData) {
+                            // update cache if different
+                            caches.open(CACHE_NAME).then((cache) => {
+                                cache.put(request, networkResponse.clone());
+                            });
+                        }
                         return networkResponse;
                     });
+                }
+
+                // no cached version. add to cache and return network response
+                return caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(request, networkResponse.clone());
+                    return networkResponse;
                 });
-            })
+            }).catch(() => cachedResponse || fetch(request));
+        })
     );
 });
